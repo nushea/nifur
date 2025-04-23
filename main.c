577059c65d8b0ae5d1f5b56503e8529ee0adc14d
Shea <nu@she-a.eu>
2025-04-23 05:13:47 +0300

    ip show

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 
 #define BUFFER 0x100
 
-
 int main(){
     printf("Content-Type: text/plain\n\n");
-	if(!(getenv("REQUEST_URI")&&getenv("SERVER_NAME"))){
+	if(!(getenv("REQUEST_URI")&&getenv("SERVER_NAME")&&getenv("REMOTE_ADDR"))){
 		printf("freaky ahh request\n");
 		return -1;
 	}
-	char uri[BUFFER], host[BUFFER];
-	strncpy(uri, getenv("REQUEST_URI"), BUFFER);
-	strncpy(host, getenv("SERVER_NAME"), BUFFER);
+	char uri[BUFFER], host[BUFFER], ip[BUFFER];
+	strncpy(uri, getenv("REQUEST_URI"), BUFFER); uri[BUFFER-1] = '\0';
+	strncpy(host, getenv("SERVER_NAME"), BUFFER); host[BUFFER-1] = '\0';
+	strncpy(ip, getenv("REMOTE_ADDR"), BUFFER); ip[BUFFER-1] = '\0';
 	
-	printf("domain: %s%s\n", host,uri);
+	printf("Domain: %s%s\nYer ip is: %s\n", host,uri, ip);
 }
--- makefile
+++ makefile
@@ -3,4 +3,4 @@ all:
 	gcc -o nifur main.c
 
 test: all
-	SERVER_NAME="subdomain.example.com" REQUEST_URI="/path/to/thing" ./nifur
+	SERVER_NAME="subdomain.example.com" REQUEST_URI="/path/to/thing" REMOTE_ADDR="127.0.0.1" ./nifur
